8754250e20cdf4fd21447279b059bc7d44c668f6
Aria Taylor <ari@aricodes.net>
2023-04-18 17:49:29 -0400

    Add colored_rect struct

---
+++ colored_rect.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
+	"github.com/faiface/pixel/pixelgl"
+)
+
+type ColoredRect struct {
+	Bounds  pixel.Rect
+	Color   pixel.RGBA
+	Surface *imdraw.IMDraw
+}
+
+func (r *ColoredRect) Contains(point pixel.Vec) bool {
+	return (point.X >= r.Bounds.Min.X &&
+		point.X <= r.Bounds.Max.X &&
+		point.Y >= r.Bounds.Min.Y &&
+		point.Y <= r.Bounds.Max.Y)
+}
+
+func (r *ColoredRect) Draw(window *pixelgl.Window) {
+	// Generate new surface if we were not provided one
+	if r.Surface == nil {
+		r.Surface = imdraw.New(nil)
+	}
+
+	r.Surface.Color = r.Color
+	r.Surface.Push(r.Bounds.Min)
+	r.Surface.Push(r.Bounds.Max)
+	r.Surface.Rectangle(0)
+
+	r.Surface.Draw(window)
+}
--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 	"golang.org/x/image/font/basicfont"
 )
 
-type coloredRect struct {
-	bounds pixel.Rect
-	color  pixel.RGBA
-}
+// type coloredRect struct {
+// 	bounds pixel.Rect
+// 	color  pixel.RGBA
+// }
 
 func main() {
 	pixelgl.Run(run)
@@ -33,41 +33,44 @@ func run() {
 
 	button := imdraw.New(nil)
 
-	var rects []coloredRect
-	var r coloredRect
+	var rects []ColoredRect = []ColoredRect{
+		{
+			Bounds: pixel.R(200, 100, 500, 300),
+			Color:  pixel.RGB(1, 0, 0),
+		},
 
-	r.bounds = pixel.R(200, 100, 500, 300)
-	r.color = pixel.RGB(1, 0, 0)
-	rects = append(rects, r)
-
-	r.bounds = pixel.R(500, 100, 600, 200)
-	r.color = pixel.RGB(0.7, 0.4, 0.2)
-	rects = append(rects, r)
-
-	for _, r := range rects {
-		pushRectToImd(r.bounds.Min, r.bounds.Max, r.color, button)
+		{
+			Bounds: pixel.R(500, 100, 600, 200),
+			Color:  pixel.RGB(0.7, 0.4, 0.2),
+		},
 	}
-	var test []time.Time
+
+	// TODO: Draw those rectangles to the window
+
+	var frameTimes []time.Time
+
+	// Moved up here so that we're not re-initializing their memory on every frame
+	basicAtlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
+	basicTxt := text.New(pixel.V(100, 100), basicAtlas)
 
 	for !win.Closed() {
 		win.Clear(pixel.RGB(0, 0, 0))
 
-		test = append(test, time.Now())
-		for test[len(test)-1].Sub(test[0]).Seconds() >= 1 {
-			test = test[1:]
+		frameTimes = append(frameTimes, time.Now())
+		for frameTimes[len(frameTimes)-1].Sub(frameTimes[0]).Seconds() >= 1 {
+			frameTimes = frameTimes[1:]
 		}
-		basicAtlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
-		basicTxt := text.New(pixel.V(100, 100), basicAtlas)
 
-		fmt.Fprintln(basicTxt, len(test))
+		fmt.Fprintln(basicTxt, len(frameTimes))
 		basicTxt.Draw(win, pixel.IM)
 
+		// TODO: Update click logic to use receiver method
 		if win.JustPressed(pixelgl.MouseButton1) {
 			mpos := win.MousePosition()
 			for i, r := range rects {
-				if mpos.X >= r.bounds.Min.X && mpos.X <= r.bounds.Max.X && mpos.Y >= r.bounds.Min.Y && mpos.Y <= r.bounds.Max.Y {
+				if mpos.X >= r.Bounds.Min.X && mpos.X <= r.Bounds.Max.X && mpos.Y >= r.Bounds.Min.Y && mpos.Y <= r.Bounds.Max.Y {
 					butt := imdraw.New(nil)
-					pushRectToImd(pixel.V((float64)(i*10), 0), pixel.V((float64)(i*10+10), 10), r.color, butt)
+					pushRectToImd(pixel.V((float64)(i*10), 0), pixel.V((float64)(i*10+10), 10), r.Color, butt)
 					butt.Draw(win)
 				}
 			}
